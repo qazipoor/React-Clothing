ab83bf87538f9593ca38ce1d025992fe88a6585b
Ali Qazipoor <qazipoor@gmail.com>
2024-04-16 13:36:33 +0300

    Migrating from redux-thunk to redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,26 +2,31 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import { thunk } from "redux-thunk";
+// import { thunk } from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from "./root-saga";
 
 import { rootReducer } from "./root-reducer";
-
-const middleWares = [
-  process.env.NODE_ENV === "development" && logger,
-  thunk,
-].filter(Boolean);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
 
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancer =
+(process.env.NODE_ENV !== "production" &&
+window &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+compose;
+
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  sagaMiddleware,
+].filter(Boolean);
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -32,5 +37,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
