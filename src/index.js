5f92b337a4d02aea3540b23acb8c405bb00b58a4
Ali Qazipoor <qazipoor@gmail.com>
2024-05-01 19:36:19 +0300

    Adding Stripe as a payment gateway

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
@@ -11,6 +12,7 @@ import App from "./App";
 
 import "./index.scss";
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,7 +20,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
